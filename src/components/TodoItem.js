8dbd7c014b5ba798f95b242660eab22e7763f301
0131ryu <131ryuji@gmail.com>
2023-02-15 13:10:12 +0900

    ass10 투두리스트 수정 기능 구현

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,50 +1,119 @@
 import { faPenNib, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useCallback, useState } from "react";
 import { todoStyle } from "./styles/todo";
 import { todoItemStyle } from "./styles/todo/todoItem";
 
-const TodoItem = ({ todo, editTodo, deleteTodo }) => {
+const TodoItem = ({ todo, deleteTodo, modifyTodo }) => {
+  const [isModify, setIsModify] = useState(false);
+  const [editText, setEditText] = useState(todo.todo);
+  const [isChecked, setIsChecked] = useState(todo.isCompleted);
+
+  const editCheckbox = useCallback(() => {
+    setIsChecked((prev) => !prev);
+    modifyTodo(todo.id, todo.todo, !isChecked);
+  }, [modifyTodo, todo.id, todo.todo, isChecked]);
+
+  const clickModify = useCallback(() => {
+    setIsModify((prev) => !prev);
+  }, []);
+
+  const modifyInputText = useCallback((e) => {
+    e.preventDefault();
+    setEditText(e.target.value);
+  }, []);
+
+  const modifySubmit = async (e) => {
+    e.preventDefault();
+    console.log("editText", editText, "todo.id", todo.id);
+    await modifyTodo(todo.id, editText, todo.isCompleted);
+    setIsModify(false);
+  };
+
   return (
     <>
-      {todo.map((t, i) => (
-        <todoStyle.Li key={i}>
-          <todoStyle.Label>
-            <input
-              type="checkbox"
-              checked={t.isCompleted}
-              onChange={(e) => {
-                editTodo(t.id, t.todo, e.target.checked);
-              }}
-            />
-            <todoStyle.Span>{t.todo}</todoStyle.Span>
+      <todoStyle.Li>
+        {isModify ? (
+          <>
+            <todoStyle.Label>
+              <input
+                type="checkbox"
+                checked={isChecked}
+                onChange={editCheckbox}
+              />
+              <form onSubmit={modifySubmit}>
+                {/* 내용 수정 */}
+                <input
+                  type="text"
+                  value={editText}
+                  onChange={modifyInputText}
+                />
+                <button
+                  className="todoapp__item-edit-btn"
+                  type="submit"
+                  data-testid="submit-button"
+                  variant="contained"
+                >
+                  제출
+                </button>
+                <button
+                  type="button"
+                  data-testid="cancel-button"
+                  onClick={() => setIsModify(false)}
+                >
+                  취소
+                </button>
+              </form>
+            </todoStyle.Label>
+          </>
+        ) : (
+          <>
+            <todoStyle.Label>
+              <input
+                type="checkbox"
+                checked={isChecked}
+                onChange={editCheckbox}
+              />
+              <todoStyle.Span>{todo.todo}</todoStyle.Span>
 
-            <todoStyle.IconPtag>
-              <todoItemStyle.Button data-testid="modify-button">
-                <FontAwesomeIcon
-                  icon={faPenNib}
-                  style={{
-                    color: "var(--light-orange)",
-                    marginLeft: "0.25rem",
+              <todoStyle.IconPtag>
+                <todoItemStyle.Button
+                  type="button"
+                  data-testid="modify-button"
+                  onClick={() => {
+                    setIsModify((prev) => !prev);
                   }}
-                />
-                수정
-              </todoItemStyle.Button>
-              <todoItemStyle.Button
-                data-testid="modify-button"
-                onClick={() => {
-                  deleteTodo(t.id);
-                }}
-              >
-                <FontAwesomeIcon
-                  icon={faTrash}
-                  style={{ color: "var(--dark-orange)", marginLeft: "0.25rem" }}
-                />
-                삭제
-              </todoItemStyle.Button>
-            </todoStyle.IconPtag>
-          </todoStyle.Label>
-        </todoStyle.Li>
-      ))}
+                >
+                  <FontAwesomeIcon
+                    icon={faPenNib}
+                    style={{
+                      color: "var(--light-orange)",
+                      marginLeft: "0.25rem",
+                    }}
+                  />
+                  수정
+                </todoItemStyle.Button>
+                <todoItemStyle.Button
+                  data-testid="delete-button"
+                  type="button"
+                  onClick={() => {
+                    deleteTodo(todo.id);
+                  }}
+                >
+                  <FontAwesomeIcon
+                    icon={faTrash}
+                    style={{
+                      color: "var(--dark-orange)",
+                      marginLeft: "0.25rem",
+                    }}
+                  />
+                  삭제
+                </todoItemStyle.Button>
+              </todoStyle.IconPtag>
+            </todoStyle.Label>
+          </>
+        )}
+      </todoStyle.Li>
     </>
   );
 };
